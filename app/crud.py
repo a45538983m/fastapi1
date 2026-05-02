58a506d49aa6146bf6b18d7748266817b9a2c081
a45538983 <a45538983@gmail.com>
2026-05-02 09:39:53 +0300

    execute

--- app/crud.py
+++ app/crud.py
@@ -14,6 +14,8 @@ def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed.decode('utf-8')
 
+
+
 def verify_password(plain: str, hashed: str) -> bool:
     return bcrypt.checkpw(plain.encode('utf-8'), hashed.encode('utf-8'))
 
